6795baa65d224a79623cccca9ed9132e1d919ea5
asajonsson <sayonsson@gmail.com>
2016-10-15 00:33:47 +0200

    Completed assignment 3

--- corr.R
+++ corr.R
@@ -1,21 +1,25 @@
 corr <- function(directory, threshold=0){
   
-  all_data = numeric()
+  ## kallar pa funktionen complete
+  complete_files <- complete(directory)
+ 
+  ## satter filtrering pa threshold
+  thresholdfilt <- subset(complete_files, nobs > threshold)
   
-  ## loop for att ga igenom filerna
-  
-  for(i in id){
-    
+  correlations = numeric()
+
+  for(i in thresholdfilt$id){
     ## satter filnamn
     filename <- paste0(directory, "/", sprintf("%03d", i), ".csv")
     
     ## satter fil
     file_data <- read.csv(filename)
-    all_data <- c(all_data, file_data[[pollutant]])
-  }
-  
-  x <- 
-  
-    cor(x, y,  method = "pearson", use = "complete.obs")
+    file_data <- file_data[complete.cases(file_data),]
     
+    file_corr <- cor(file_data[["sulfate"]], file_data[["nitrate"]])
+
+    correlations <- c(correlations, file_corr)
   }
+  
+  return(correlations)
+}
